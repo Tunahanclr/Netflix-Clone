35666d2f0547b51bc02b635c0af3192c95fb22a8
Tunahanclr <tunaceler@gmail.com>
2023-03-30 22:22:21 +0300

    responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,32 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
+
 export default function Home() {
   const [movies,setMovies]=useState([])
-
-    const movie= movies[Math.floor(Math.random()* movies.length)]
+  const movie= movies[Math.floor(Math.random()* movies.length)]
 
   useEffect(()=>{
-        axios.get(requests.requestPopular).then((response)=>{
-          setMovies(response.data.results)
-        })
+    axios.get(requests.requestPopular).then((response)=>{
+      setMovies(response.data.results)
+    })
   },[])
-  console.log(movie)
+
   return (
-    <div className='w-full h-[95vh] text-white'>
-        <div className='w-full h-full'>  
-        <div className='absolute w-full h-[95vh] object-cover bg-gradient-to-tr from-black'></div>
-        <img className='h-full w-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-        <div className='absolute  w-full top-[30%] p-4 md:p-8'>
-          <h1 className=' text-4xl font-bold  md:text-6xl lg:text-7xl w-[80%] lg:w-[1000px] '>{movie?.title}</h1>
-          <p className=' w-[300px] text-xs md:text-base md:w-[700px] mt-4'>{movie?.overview}</p>
-        <div className='mt-5'>
-          <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
-          <button className='border bg-gray-300 text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
-   </div>
+    <div className='w-full h-screen text-white'>
+      <div className='w-full h-full'>  
+        <div className='absolute w-full h-screen object-cover bg-gradient-to-tr from-black'></div>
+        <img className='h-full w-full object-cover object-center' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+        <div className='absolute w-full top-[30%] p-4 md:p-8'>
+          <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl w-[80%] lg:w-[1000px] '>{movie?.title}</h1>
+          <p className=' w-[300px] sm:w-[500px] text-xs sm:text-base md:w-[700px] mt-4'>{movie?.overview}</p>
+          <div className='mt-5'>
+            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+            <button className='border bg-gray-300 text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
+    </div>
   )
 }
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,55 @@ export default function Navbar() {
   }
   return (
    <>
-    <div className='flex items-center justify-between  pl-16 pr-16 absolute p-4 z-50px  w-full '>
+   
+    <div className='flex items-center justify-between pl-8   lg:pl-16 lg:pr-16 pr-10 absolute p-4 z-50px  w-full '>
 <div className='flex items-center gap-10'>
- <h1  className='text-red-600 text-4xl font-bold cursor-pointer z-50'>NETFLİX</h1>
-<div className='flex items-center gap-4 z-50'>
+ <h1  className='text-red-600 text-3xl md:text-4xl  font-bold cursor-pointer z-50'>NETFLİX</h1>
+<div>
+<div className="lg:hidden relative  -space-x-8 z-50">
+        <label htmlFor="mobile-menu" className="sr-only">
+        </label>
+        <select
+          id="mobile-menu"
+          name="mobile-menu"
+          className="block appearance-none w-36 px-4 py-2  rounded-lg  focus:outline-none  bg-black/25 text-sm  text-white  font-bold "
+        >
+          <option value="a" selected><a className='text-white text-sm  transition-all font-semibold' href="">Ana Sayfa</a>
+</option>
+          <option value="b"> 
+ <a className='text-white text-sm hover:opacity-70 transition-all' href="">Diziler</a>
+           </option>
+          <option value="c">
+ <a className='text-white text-sm hover:opacity-70 transition-all' href="">Filmler</a>
+          </option>
+          <option value="d">
+ <a className='text-white text-sm hover:opacity-70 transition-all' href="">Yeni ve Popüler</a>
+          </option>
+          <option value="e">
+ <a className='text-white text-sm hover:opacity-70 transition-all' href="">Listem</a>
+          </option>
+          <option value="f">
+ <a className='text-white text-sm hover:opacity-70 transition-all' href="">Dile Göre Göz At</a>
+
+          </option>
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg
+            className="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+</div>
+<div className=' lg:items-center hidden lg:flex lg:whitespace-nowrap lg:gap-4 lg:z-50'>
 <a className='text-white text-sm  transition-all font-semibold' href="">Ana Sayfa</a>
  <a className='text-white text-sm hover:opacity-70 transition-all' href="">Diziler</a>
  <a className='text-white text-sm hover:opacity-70 transition-all' href="">Filmler</a>
@@ -26,8 +71,9 @@ export default function Navbar() {
 </div>
 
         <div className='z-50'>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer hover:bg-red-700  text-white z-50' onClick={logout}>Log out</button>
+            <button className='bg-red-600 px-2 lg:px-6 py-2 rounded cursor-pointer hover:bg-red-700 whitespace-nowrap   text-white z-50' onClick={logout}>Log out</button>
         </div>
+      
     </div>
     <Main/>
 
